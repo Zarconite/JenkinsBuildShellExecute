83260432945b2c46101d81ee7d0bd3fcd4bb7866
Zarconite <zarconite@gmail.com>
2019-01-09 15:21:54 +0000

    Working version of my Build execute shell script for Springboot

---
+++ buildscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PROJECT="spring-boot-hello-world"
+INSTALL_FOLDER="$JENKINS_HOME/$PROJECT"
+mvn clean package
+
+#Kill the old one
+PID_LOCATION="$INSTALL_FOLDER/PID"
+OLD_PID=$(cat $PID_LOCATION)
+kill -9 $OLD_PID
+
+#Make the folder
+mkdir -p $INSTALL_FOLDER
+rm -rf $INSTALL_FOLDER/*
+
+#Copy the Jar
+JAR="$(ls target/ | grep .jar | grep -v .original)"
+cp target/$JAR $INSTALL_FOLDER/$JAR
+
+#Run the Jar
+BUILD_ID=dontKillMe java -jar $INSTALL_FOLDER/$JAR 2>&1 &
+echo $! >$PID_LOCATION
+disown
